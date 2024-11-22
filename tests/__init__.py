cc8eb2858ddb76b93cf2402745ebd669012535a2
SHASHANK G.G <suyogshashank@gmail.com>
2024-11-22 17:18:23 +0530

    add unit test cases for users route methods

---
+++ tests/__init__.py
@@ -0,0 +1,61 @@
+import pytest
+from auth_app.services import UserService
+
+
+
+@pytest.fixture
+def override_token_factory():
+    return ("testuser", 1)
+
+
+
+
+# some dummy expired bearer
+BEARER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0dXNlciIsInJvbGUiOjIsImV4cCI6MTczMjAxODY2MSwidHlwZSI6ImFjY2VzcyJ9.npfx4CYhZOF618i21pNDdqIIM68AC0ezs6KYkhw8nQ4'
+
+
+@pytest.fixture
+def override_get_current_user():
+
+    return {
+        "id": "1141",
+        "username": "testuser",
+        "email" : "test@gmail.com",
+        "password" : "sadfasdfdsfdasfsdfadf",
+        "role_id"  : 2,
+        "twofa_status": 0
+    }
+
+
+
+class MockUserRepository:
+    async def get_user_details(self, username: str):
+        user_details = {
+                    "stat": "Ok",
+                    "Result": {
+                                "username": "testuser",
+                                "email": "test@gmail.com",
+                                "role_name": "admin"
+                                }
+                    }
+        
+        return user_details
+    
+
+    async def assign_role(self, username, role_name):
+        return True
+    
+
+
+@pytest.fixture    
+def override_get_user_service():
+   mock_user_repo = MockUserRepository()
+
+   return UserService(user_repo=mock_user_repo)
+
+
+
+
+
+__all__ = ["override_get_user_service", "MockUserRepository", "override_get_current_user", "override_token_factory",
+           "BEARER_TOKEN"]
--- tests/routers/test_user_routes.py
+++ tests/routers/test_user_routes.py
@@ -1,61 +1,15 @@
 import pytest
 from fastapi.testclient import TestClient
-from unittest.mock import MagicMock, AsyncMock, Mock
+from unittest.mock import MagicMock, AsyncMock, Mock, patch
 from auth_app import app
 from auth_app.services import UserService, get_user_service   
 from auth_app.managers import RoleManager,UserRole, UserManager
-from auth_app.utils import TokenFactory
+from auth_app.utils import TokenFactory, TokenManager
 from auth_app.repositories import UserRepository
 from auth_app.schemas import User
-
-
-@pytest.fixture
-def override_token_factory():
-    return {"username": "testuser", "role": "admin"}
-
-
-class MockUserRepository:
-    async def get_user_details(self, username: str):
-        user_details = {
-                    "stat": "Ok",
-                    "Result": {
-                                "username": "testuser",
-                                "email": "test@gmail.com",
-                                "role_name": "admin"
-                                }
-                    }
-        
-        return user_details
-    
-
-    async def assign_role(self, username, role_name):
-        return True
-    
-
-@pytest.fixture
-def override_allowed_action():
-    return lambda action: True  
-    
-
-@pytest.fixture    
-def override_get_user_service():
-   mock_user_repo = MockUserRepository()
-
-   return UserService(user_repo=mock_user_repo)
-
-
-@pytest.fixture
-def override_get_current_user():
-
-    return {
-        "id": "1141",
-        "username": "testuser",
-        "email" : "test@gmail.com",
-        "password" : "sadfasdfdsfdasfsdfadf",
-        "role_id"  : 2,
-        "twofa_status": 0
-    }
-
+from tests import BEARER_TOKEN, override_token_factory, override_get_user_service, override_get_current_user
+from tests.routers import override_allowed_action
+from fastapi import HTTPException, status
 
 
 @pytest.fixture
@@ -63,7 +17,7 @@ def override_dependencies(override_token_factory, override_get_user_service,
                           override_get_current_user, override_allowed_action):
     
     app.dependency_overrides[get_user_service] = lambda:override_get_user_service
-    # app.dependency_overrides[RoleManager.allowed_action] = override_allowed_action
+    app.dependency_overrides[RoleManager.allowed_action] = override_allowed_action
     app.dependency_overrides[TokenFactory.validate_token] = lambda: override_token_factory
     app.dependency_overrides[UserManager.get_current_user] = lambda : override_get_current_user
 
@@ -77,134 +31,282 @@ def client():
     return TestClient(app)
 
 
+@pytest.mark.usefixtures("override_dependencies")
+class TestAssignRole:
 
+    def test_assign_role_success(self,client):
 
+        """Test for assigning roles to users."""
 
+        response = client.put('/user/assign-role/', 
+        data={"username":"testuser", "role_name":"admin"},
+        headers={"Authorization": f"Bearer {BEARER_TOKEN}"})
 
+        assert response.status_code == 200
 
+        data = response.json()
 
+        assert data['stat'] == 'Ok'
+        assert data['Result'] == "'admin' role_name for 'testuser' updated successfully! "
 
 
-def test_read_users_me(client, override_dependencies):
+    def test_assign_role_user_not_found(self,client):
 
-    """Test for getting user details."""
+        """Test for assigning roles to users."""
 
-    # Call the endpoint with a sample username
+        with patch('auth_app.services.UserService.assign_role',new=AsyncMock(side_effect= HTTPException(status.HTTP_404_NOT_FOUND, detail="User not found"))):
+            
+            response = client.put('/user/assign-role/',
+            data={"username":"testuser", "role_name":"superuser"},
+            headers={"Authorization": f"Bearer {BEARER_TOKEN}"})
 
-    response = client.get('/user/read/users/')
+        assert response.status_code == 404
 
-    # Assert that the status code is 200 OK
+        data = response.json()
 
-    # assert response.json() == {}
-    assert response.status_code == 200
+        assert data['stat'] == 'Not_Ok'
+        assert data['Reason'] == "User not found"
 
-    # Assert that the response JSON matches the expected structure
 
-    data = response.json()
+    def test_assign_role_expired_token(self,client):
 
-    assert data['username'] == 'testuser'
-    assert data['email'] == 'test@gmail.com'
+        """Test for assigning roles to users."""
 
+        # del app.dependency_overrides[RoleManager.allowed_action]
+        del app.dependency_overrides[TokenFactory.validate_token] 
 
+        
+        with patch('auth_app.utils.TokenManager.is_token_blacklisted', new=AsyncMock(return_value={"token": BEARER_TOKEN})):    
+            response = client.put('/user/assign-role/',
+            data={"username":"testuser", "role_name":"admin"},
+            headers={"Authorization": f"Bearer {BEARER_TOKEN}"})
 
-# def test_read_users_me_expired_token(client, override_dependencies):
+        assert response.status_code == 401
 
-#     """Test for getting user details."""
+        data = response.json()
 
-#     # Call the endpoint with a sample username
+        assert data['stat'] == 'Not_Ok'
+        assert data['Reason'] == "User logged out. Please re-login"
 
-#     del app.dependency_overrides[TokenFactory.validate_token]
-#     del app.dependency_overrides[UserManager.get_current_user]
 
-#     # some dummy expired bearer
-#     bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0dXNlciIsInJvbGUiOjIsImV4cCI6MTczMjAxODY2MSwidHlwZSI6ImFjY2VzcyJ9.npfx4CYhZOF618i21pNDdqIIM68AC0ezs6KYkhw8nQ4'
+    def test_assign_role_internal_server_error(self,client):
 
-#     response = client.get('/user/read/users/', headers={"Authorization": f"Bearer {bearer}"})
+        """Test for assigning roles to users."""
 
-#     # Assert that the status code is 401 Not_OK
+        del app.dependency_overrides[RoleManager.allowed_action]
+        del app.dependency_overrides[TokenFactory.validate_token] 
+            
+        response = client.put('/user/assign-role/',
+        data={"username":"testuser", "role_name":"admin"},
+        headers={"Authorization": f"Bearer {BEARER_TOKEN}"})
 
-#     assert response.status_code == 401
+        assert response.status_code == 500
 
-#     # Assert that the response JSON matches the expected structure
+        data = response.json()
 
-#     data = response.json()
+        assert data['stat'] == 'Not_Ok'
+        assert data['Reason'] == "Internal Server Error"
 
-#     assert data['stat'] == 'Not_Ok'
-#     assert data['Reason'] == 'User logged out. Please re-login'
 
 
+@pytest.mark.usefixtures("override_dependencies")
+class TestReadUsersMe:
 
-def test_get_user_details(client, override_dependencies):
+    def test_read_users_me_success(self,client):
 
-    """Test for getting user details."""
+        """Test for getting user details."""
 
-    # Call the endpoint with a sample username
+        # Call the endpoint with a sample username
 
-    response = client.get('/user/get-user-details/?username=testuser')
+        response = client.get('/user/read/users/')
 
-    # Assert that the status code is 200 OK
+        # Assert that the status code is 200 OK
 
-    assert response.status_code == 200
+        # assert response.json() == {}
+        assert response.status_code == 200
 
-    # Assert that the response JSON matches the expected structure
+        # Assert that the response JSON matches the expected structure
 
-    data = response.json()
+        data = response.json()
 
-    assert data['stat'] == 'Ok'
-    assert data['Result']['username'] == 'testuser'
-    assert data['Result']['email'] == 'test@gmail.com'
-    assert data['Result']['role_name'] == 'admin'
+        assert data['username'] == 'testuser'
+        assert data['email'] == 'test@gmail.com'
 
 
 
+    def test_read_users_me_user_not_found(self,client):
 
-def test_get_user_details_no_token(client, override_dependencies):
+        """Test for getting user details."""
 
-    """Test for getting user details."""
+        # Call the endpoint with a sample username
 
-    # Call the endpoint with a sample username
+        del app.dependency_overrides[UserManager.get_current_user]
 
-    del app.dependency_overrides[TokenFactory.validate_token]
+        with patch('auth_app.managers.UserManager.user_query', new=AsyncMock(return_value=None)):
+            response = client.get('/user/read/users/', headers={"Authorization": f"Bearer {BEARER_TOKEN}"})
 
-    response = client.get('/user/get-user-details/?username=testuser')
 
-    # Assert that the status code is 401 Not_OK
+        # Assert that the status code is 404 Not_OK
 
-    assert response.status_code == 401
+        assert response.status_code == 404
 
-    # Assert that the response JSON matches the expected structure
+        # Assert that the response JSON matches the expected structure
 
-    data = response.json()
+        data = response.json()
 
-    assert data['stat'] == 'Not_Ok'
-    assert data['Reason'] == 'Not authenticated'
+        assert data['stat'] == 'Not_Ok'
+        assert data['Reason'] == 'User not found'
 
 
 
 
-# def test_get_user_details_expired_token(client, override_dependencies):
+    def test_read_users_me_expired_token(self,client):
 
-#     """Test for getting user details."""
+        """Test for getting user details."""
 
-#     # Call the endpoint with a sample username
+        # Call the endpoint with a sample username
 
-#     del app.dependency_overrides[TokenFactory.validate_token]
+        del app.dependency_overrides[TokenFactory.validate_token]
+        del app.dependency_overrides[UserManager.get_current_user]
 
-#     # some dummy expired bearer
-#     bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0dXNlciIsInJvbGUiOjIsImV4cCI6MTczMjAxODY2MSwidHlwZSI6ImFjY2VzcyJ9.npfx4CYhZOF618i21pNDdqIIM68AC0ezs6KYkhw8nQ4'
 
-#     response = client.get('/user/get-user-details/?username=testuser', headers={"Authorization": f"Bearer {bearer}"})
+        with patch('auth_app.utils.TokenManager.is_token_blacklisted', new=AsyncMock(return_value={"token": BEARER_TOKEN})):
+            response = client.get('/user/read/users/', headers={"Authorization": f"Bearer {BEARER_TOKEN}"})
 
-#     # Assert that the status code is 401 Not_OK
+        # Assert that the status code is 401 Not_OK
 
-#     assert response.status_code == 401
+        assert response.status_code == 401
 
-#     # Assert that the response JSON matches the expected structure
+        # Assert that the response JSON matches the expected structure
 
-#     data = response.json()
+        data = response.json()
 
-#     assert data['stat'] == 'Not_Ok'
-#     assert data['Reason'] == 'User logged out. Please re-login'
+        assert data['stat'] == 'Not_Ok'
+        assert data['Reason'] == 'User logged out. Please re-login'
+
+
+
+    def test_read_users_me_internal_server_error(self,client):
+
+        """Test for getting user details."""
+
+        # Call the endpoint with a sample username
+
+        # del app.dependency_overrides[TokenFactory.validate_token]
+        del app.dependency_overrides[UserManager.get_current_user]
+
+       
+        response = client.get('/user/read/users/', headers={"Authorization": f"Bearer {BEARER_TOKEN}"})
+
+        # Assert that the status code is 500 Not_OK
+
+        assert response.status_code == 500
+
+        # Assert that the response JSON matches the expected structure
+
+        data = response.json()
+
+        assert data['stat'] == 'Not_Ok'
+        assert data['Reason'] == 'Internal Server Error'
+
+
+
+@pytest.mark.usefixtures("override_dependencies")
+class TestGetUserDetails:
+
+    def test_get_user_details_success(self,client):
+
+        """Test for getting user details."""
+
+        # Call the endpoint with a sample username
+
+        response = client.get('/user/get-user-details/?username=testuser')
+
+        # Assert that the status code is 200 OK
+
+        assert response.status_code == 200
+
+        # Assert that the response JSON matches the expected structure
+
+        data = response.json()
+
+        assert data['stat'] == 'Ok'
+        assert data['Result']['username'] == 'testuser'
+        assert data['Result']['email'] == 'test@gmail.com'
+        assert data['Result']['role_name'] == 'admin'
+
+
+
+
+    def test_get_user_details_no_token(self, client):
+
+        """Test for getting user details."""
+
+        # Call the endpoint with a sample username
+
+        del app.dependency_overrides[TokenFactory.validate_token]
+
+        response = client.get('/user/get-user-details/?username=testuser')
+
+        # Assert that the status code is 401 Not_OK
+
+        assert response.status_code == 401
+
+        # Assert that the response JSON matches the expected structure
+
+        data = response.json()
+
+        assert data['stat'] == 'Not_Ok'
+        assert data['Reason'] == 'Not authenticated'
+
+
+
+    def test_get_user_details_expired_token(self, client):
+
+        """Test for getting user details."""
+
+        # Call the endpoint with a sample username
+
+        del app.dependency_overrides[TokenFactory.validate_token]
+        del app.dependency_overrides[UserManager.get_current_user]
+
+
+        with patch('auth_app.utils.TokenManager.is_token_blacklisted', new=AsyncMock(return_value={"token": BEARER_TOKEN})):
+            response = client.get('/user/get-user-details/?username=testuser', headers={"Authorization": f"Bearer {BEARER_TOKEN}"})
+
+        # Assert that the status code is 401 Not_OK
+
+        assert response.status_code == 401
+
+        # Assert that the response JSON matches the expected structure
+
+        data = response.json()
+
+        assert data['stat'] == 'Not_Ok'
+        assert data['Reason'] == 'User logged out. Please re-login'
+
+
+
+    def test_get_user_details_internal_server_error(self,client):
+
+        """Test for getting user details."""
+
+        # Call the endpoint with a sample username
+
+        del app.dependency_overrides[TokenFactory.validate_token]
+       
+        response = client.get('/user/get-user-details/?username=testuser', headers={"Authorization": f"Bearer {BEARER_TOKEN}"})
+
+        # Assert that the status code is 500 Not_OK
+
+        assert response.status_code == 500
+
+        # Assert that the response JSON matches the expected structure
+
+        data = response.json()
+
+        assert data['stat'] == 'Not_Ok'
+        assert data['Reason'] == 'Internal Server Error'
 
 
 
